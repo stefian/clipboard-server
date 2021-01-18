90ee03241420f47e139b45551b7c20c24bb657a7
stefian <v_ianta@yahoo.com>
2021-01-18 10:06:17 -0400

    + // middleware // app.use(logger('dev')); app.use(bodyParser.json()); app.use(bodyParser.urlencoded({ extended: false })); app.use(express.static(path.join(__dirname, 'public')));

--- app.js
+++ app.js
@@ -6,7 +6,12 @@ const redis = require('redis');
 
 const app = express();
 
-// view engine setup //
+// ejs view engine setup //
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
+// middleware //
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, 'public')));

b3cd500d83e6b2398243912f202db2b0fa120efb
stefian <v_ianta@yahoo.com>
2021-01-18 09:59:02 -0400

    + const app = express();

--- app.js
+++ app.js
@@ -1,8 +1,9 @@
 const express = require('express');
 const path = require('path');
 const logger = require('morgan');
-const cookieParser = require('cookie-parser');
 const bodyParser = require('body-parser');
 const redis = require('redis');
+
+const app = express();
 
 
